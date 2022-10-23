dc9c42232f110a09bd9ad8c85004629918cba832
Johan Selin <johanselin99@gmail.com>
2022-10-24 00:16:01 +0200

    refactored to use chess positions for squares instead of a simple int

--- lib/chess_view.dart
+++ lib/chess_view.dart
@@ -12,65 +12,60 @@ class _BoardWidgetState extends State<BoardWidget> {
   final Color blackPiecesColor = Colors.black;
   final Color selectColor = Colors.yellow;
 
-  int selected = 0;
+  String selected = '';
 
   double squareSize(BuildContext context) => MediaQuery.of(context).size.width / 8;
 
-  bool isSelected(int id) {
-    return selected == id;
+  bool isSelected(String position) {
+    return selected == position;
   }
 
-  Color getColor(cellId) {
+  Color getColor(String position) {
     int columnOffset = 0;
-    if ((cellId > 8 && cellId <= 16) || (cellId > 24 && cellId <= 32) || (cellId > 40 && cellId <= 48) || (cellId > 56)) {
+    if (columnIsBDFH(position)) {
       columnOffset = 1;
     }
-    Color color = getCorrectBaseColorFromPosition(cellId, columnOffset);
+    Color color = getCorrectBaseColorFromPosition(position, columnOffset);
 
-    if (isSelected(cellId)) {
+    if (isSelected(position)) {
       return Color.alphaBlend(Colors.yellow.withOpacity(0.4), color);
     }
     return color;
   }
 
-  Color getCorrectBaseColorFromPosition(id, int columnOffset) {
-    Color color = (id + columnOffset) % 2 == 0 ? blackPiecesColor : whitePiecesColor;
+  Color getCorrectBaseColorFromPosition(position, int columnOffset) {
+    Color color = (int.parse(position[1]) + columnOffset) % 2 == 0 ? blackPiecesColor : whitePiecesColor;
     return color;
   }
 
-  Column generateColumn(bool even, Color color1, Color color2, double size, int startNumber) {
-    if (even) {
+  Column generateColumn(Color color1, Color color2, double size, String column) {
+    if (columnIsBDFH(column)) {
       Color tmp = color1;
       color1 = color2;
       color2 = tmp;
     }
-
-    int cellIdStart = (startNumber * 8);
+    List<Widget> containers = [];
+    for (var i = 8; i >= 1; i--) {
+      containers.add(generateContainer(color1, size, '$column$i'));
+    }
 
     return Column(
-      children: [
-        generateContainer(color1, size, cellIdStart + 1),
-        generateContainer(color2, size, cellIdStart + 2),
-        generateContainer(color1, size, cellIdStart + 3),
-        generateContainer(color2, size, cellIdStart + 4),
-        generateContainer(color1, size, cellIdStart + 5),
-        generateContainer(color2, size, cellIdStart + 6),
-        generateContainer(color1, size, cellIdStart + 7),
-        generateContainer(color2, size, cellIdStart + 8),
-      ],
+      children: containers,
     );
   }
 
-  GestureDetector generateContainer(Color color, double size, int id) {
+  bool columnIsBDFH(String column) => column.contains(RegExp('[bBdDfFhH]'));
+
+  GestureDetector generateContainer(Color color, double size, String position) {
     return GestureDetector(
       onTap: () {
-        debugPrint('$id');
+        debugPrint(position);
         setState(() {
-          selected = selected == id ? 0 : id;
+          selected = selected == position ? '' : position;
         });
       },
       child: Container(
-        color: getColor(id),
+        color: getColor(position),
         width: size,
         height: size,
       ),
@@ -79,9 +74,11 @@ class _BoardWidgetState extends State<BoardWidget> {
 
   Row generateBoard(Color color1, Color color2, double size) {
     List<Column> columns = [];
-    for (var i = 0; i < 8; i++) {
-      columns.add(generateColumn(i % 2 == 0, color1, color2, size, i));
+
+    for (var column in <String>['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']) {
+      columns.add(generateColumn(color1, color2, size, column));
     }
+
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: columns,
